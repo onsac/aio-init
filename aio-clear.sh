2bdaca79359e828a5386f4cff53cdd383cfbab9b
ronaldoMoreira <ronaldo.moreira@inforegis.com.br>
2021-01-13 11:49:42 -0300

    prod2

--- aio-clear.sh
+++ aio-clear.sh
@@ -4,5 +4,5 @@ echo "AIO INTEGRADOR 2.0 - Clear SO"
 echo "======================================================================================"
 rm /tmp/steps.txt
 rm /tmp/.common.lib
-su aio -c 'pm2 kill'
+su - aio -c 'pm2 kill'
 userdel -r aio

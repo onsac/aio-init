6b149311ec89b0a0d6cff2c5fb00ef5bbf02f967
ronaldoMoreira <ronaldo.moreira@inforegis.com.br>
2021-01-13 09:30:09 -0300

    prod

--- aio-init.sh
+++ aio-init.sh
@@ -231,7 +231,9 @@ fi
 
 check_step 37
 if [ "$?" -eq "0" ]; then
-   pw2 | sudo -S ${STARTUP} && set_step 37 "set pm2-startup OK" || echo "37 - set pm2-startup failed" 
+sudo -s <<EOF
+${STARTUP} && set_step 37 "set pm2-startup OK" || echo "37 - set pm2-startup failed"
+EOF    
 fi
 
 

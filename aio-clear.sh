4e2b26b1467e4d39c4fc78b641d21b348f779677
ronaldoMoreira <ronaldo.moreira@inforegis.com.br>
2021-01-13 15:20:49 -0300

    prod2

--- aio-clear.sh
+++ aio-clear.sh
@@ -20,24 +20,24 @@ get_common
 
 check_step C001
 if [ "$?" -eq "0" ]; then
-   echo "" && set_step C001  "get common OK" || echo "C001 - get common failed"
+   echo "" && set_step C001 "get common OK" || stop_step C001 "get common failed"
 fi
 
 check_step C002
 if [ "$?" -eq "0" ]; then
-   echo "" && set_step C002 "set common OK" || echo "C002 - set common failed"
+   echo "" && set_step C002 "set common OK" || stop_step C002 "set common failed"
 fi
 
 check_step C003
 if [ "$?" -eq "0" ]; then
-   is_root_user && set_step C003 "Usuário root OK" || echo "C003 - Usuário root failed"
+   is_root_user && set_step C003 "Usuário root OK" || stop_step C003 "Usuário root failed"
 fi
 
 check_step C004
 if [ "$?" -eq "0" ]; then
    CONTKILL=$(pkill -c $USER)
    if [ "$CONTKILL" -gt "0" ]; then
-      pkill -u $USER && set_step C004 "kill OK" || echo "C004 - kill failed"
+      pkill -u $USER && set_step C004 "kill OK" || stop_step C004 "kill failed"
    else
       set_step C004 "kill OK"
    fi
@@ -45,7 +45,7 @@ fi
 
 check_step C005
 if [ "$?" -eq "0" ]; then
-   userdel -r $USER && set_step C005 "userdel OK" || echo "C005 - userdel failed"
+   userdel -r $USER && set_step C005 "userdel OK" || stop_step C005 "userdel failed"
 fi
 
 check_step C006

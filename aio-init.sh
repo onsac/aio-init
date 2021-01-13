6e4ef7649e7bcec3ae2825657ff88129f8fcebf4
ronaldoMoreira <ronaldo.moreira@inforegis.com.br>
2021-01-13 09:47:34 -0300

    prod

--- aio-init.sh
+++ aio-init.sh
@@ -231,7 +231,7 @@ fi
 
 check_step 37
 if [ "$?" -eq "0" ]; then
-sudo su - <<'EOF'
+sudo -s <<'EOF'
 "${STARTUP}" && set_step 37 "set pm2-startup OK" || echo "37 - set pm2-startup failed"
 EOF
 fi

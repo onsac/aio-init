85e6c6d129e5c3cb41b0d4f0ab9b0202f376e06f
ronaldoMoreira <ronaldo.moreira@inforegis.com.br>
2021-01-19 17:05:47 -0300

    prod

--- aio-init.sh
+++ aio-init.sh
@@ -82,9 +82,9 @@ if [ "$?" -eq "0" ]; then
 else
    sudo su - $USER 
 fi
-
+SETEP_ANT=$STEP
 . /tmp/.common.lib
-
+STEP=$SETEP_ANT
 check_step
 if [ "$?" -eq "0" ]; then
    echo "" && set_step "set common" || stop_step "set common"

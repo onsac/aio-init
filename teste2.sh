25cb5f0272ebc974710dde09ed73542cd1b30b70
ronaldoMoreira <ronaldo.moreira@inforegis.com.br>
2021-01-19 08:30:13 -0300

    prod

--- teste2.sh
+++ teste2.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+print_line()
+{
+i=1
+LINE=""
+columns=$(tput cols)
+while [ "$i" -lt "$columns" ]
+do
+  LINE=$LINE"="
+  ((i=i+1))
+done
+echo $LINE
+}
+
+print_line
+echo "AIO INTEGRADOR 2.0 - Setup SO"
+print_line
+declare -r LTRUE=0
+declare -r LFALSE=1
 get_common(){
    wget --no-cache --no-cookies --no-check-certificate -O /tmp/.common.lib http://raw.githubusercontent.com/onsac/aio-init/main/common.lib 2>/dev/null
    if [ "$?" -ne "0" ]; then
@@ -8,14 +26,5 @@ get_common(){
    fi
 }
 
-get_common && echo "" || echo "get common failed"; exit 1
-
-. /tmp/.common.lib 
 
 print_line
-echo "AIO INTEGRADOR 2.0 - SETUP"
-print_line
-
-declare -r LTRUE=0
-declare -r LFALSE=1
-
